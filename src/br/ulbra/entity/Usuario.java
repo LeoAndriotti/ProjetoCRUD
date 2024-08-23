ba95575ea3748b3016f32bb7a6ee12564fb1b8d1
LeoAndriotti <168148288+LeoAndriotti@users.noreply.github.com>
2024-08-22 22:12:05 -0300

    Add files via upload

---
+++ src/br/ulbra/entity/Usuario.java
@@ -0,0 +1,66 @@
+
+package br.ulbra.entity;
+
+public class Usuario {
+    private int idUsu;
+    private String nomeUsu;
+    private String emailUsu;
+    private String senhaUsu;
+    private String foneUsu;
+    private String sexoUsu;
+    
+    public Usuario(){
+    }
+    public String toString(){
+        return "ID: "+this.idUsu+" Nome: "+this.nomeUsu+" E-mail: "+this.emailUsu+" Senha: "+this.senhaUsu+" Fone: "+this.foneUsu+" Sexo: "+this.sexoUsu;
+    }
+
+    public int getIdUsu() {
+        return idUsu;
+    }
+
+    public void setIdUsu(int idUsu) {
+        this.idUsu = idUsu;
+    }
+
+    public String getNomeUsu() {
+        return nomeUsu;
+    }
+
+    public void setNomeUsu(String nomeUsu) {
+        this.nomeUsu = nomeUsu;
+    }
+
+    public String getEmailUsu() {
+        return emailUsu;
+    }
+
+    public void setEmailUsu(String emailUsu) {
+        this.emailUsu = emailUsu;
+    }
+
+    public String getSenhaUsu() {
+        return senhaUsu;
+    }
+
+    public void setSenhaUsu(String senhaUsu) {
+        this.senhaUsu = senhaUsu;
+    }
+
+    public String getFoneUsu() {
+        return foneUsu;
+    }
+
+    public void setFoneUsu(String foneUsu) {
+        this.foneUsu = foneUsu;
+    }
+
+    public String getSexoUsu() {
+        return sexoUsu;
+    }
+
+    public void setSexoUsu(String sexoUsu) {
+        this.sexoUsu = sexoUsu;
+    }
+  
+}
